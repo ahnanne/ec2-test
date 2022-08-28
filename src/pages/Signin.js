86d46dd1d035e2ff68c1e79a776335276ef64668
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-08-28 15:06:43 +0900

    [A] github 소셜로그인 기능 추가

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,11 +1,64 @@
-// https://creativedesignsguru.com/next-js-supabase-auth/
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { supabase } from '../utils/SupabaseClient';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  height: 100vh;
+  padding: 40px 20px 0;
+`;
+
+const StyledButton = styled.button`
+  cursor: pointer;
+  width: 300px;
+  height: 40px;
+  background-color: #ffffff;
+  color: #1a1a1a;
+  font-size: 16px;
+  font-weight: 700;
+  transition: all 200ms;
+
+  &:focus,
+  &:active {
+    transform: translateY(3px);
+    background-color: #61dafb;
+  }
+`;
 
 const Signin = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (supabase.auth.user()) {
+      navigate('/', { replace: true });
+    }
+  }, [ supabase ]);
+
+  const handleSignin = async () => {
+    const { error } = await supabase.auth.signIn(
+      {
+        provider: 'github',
+      },
+      {
+        redirectTo: 'http://localhost:3000/callback',
+      },
+    );
+
+    // TODO: toastify
+    if (error) {
+      alert(JSON.stringify(error));
+    }
+  };
+
   return (
-    <div>
-      <p>hello..</p>
-    </div>
-  )
+    <StyledContainer>
+      <StyledButton type="button" onClick={handleSignin}>
+        Sign In with GitHub
+      </StyledButton>
+    </StyledContainer>
+  );
 };
 
 export default Signin;
+// https://creativedesignsguru.com/next-js-supabase-auth/
+// https://supabase.com/docs/guides/auth/auth-github
