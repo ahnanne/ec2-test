635975c1ec13a05953c21a4c39d887f7924d987e
ahnanne <54733637+ahnanne@users.noreply.github.com>
2022-08-28 15:11:04 +0900

    [U] 로그인한 상태에선 로그인 페이지 이동 방지

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
-import logo from '../logo.svg';
-
+import { supabase } from '../utils/SupabaseClient';
 import { Link } from 'react-router-dom';
 
+import logo from '../logo.svg';
+
 const Home = () => {
+  const preventRoute = () => {
+    if (supabase.auth.user()) {
+      // TODO: toastify
+      alert('이미 로그인하셨어용');
+    }
+  };
+
   return (
     <div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/signin" className='signin-link'>
+        <Link to="/signin" className='signin-link' onClick={preventRoute}>
           로그인
         </Link>
       </header>
